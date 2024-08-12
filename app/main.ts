1559ab2306ac81ad25b8dad44ddb0587b623741e
Gitanshu Talwar <gitanshutalwar@gmail.com>
2024-08-12 18:12:31 +0530

    test

--- app/main.ts
+++ app/main.ts
@@ -10,7 +10,7 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
   connection.on("data", (data) => {
-    const dataString = data.toString()
+    const dataString = data.toString().trim();
     console.log(dataString)
     const command = dataString.split(" ")[0].trim();
     console.log("="+command+"=")
