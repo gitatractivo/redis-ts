7b25b684e3f7f072308d6d07c9967617414b607c
Gitanshu Talwar <gitanshutalwar@gmail.com>
2024-08-12 18:09:34 +0530

    test

--- app/main.ts
+++ app/main.ts
@@ -13,7 +13,7 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
     const dataString = data.toString()
     console.log(dataString)
     const command = dataString.split(" ")[0].trim();
-    console.log(command, Commands.PING, Commands.PING===command);
+    console.log(command, Commands.PING, "PING"===command);
     switch(command){
       case "PING":
         console.log("inside:PING")
