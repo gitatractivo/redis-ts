79c51aa37e1a0d1ce961b9d53878123875f43b3c
Gitanshu Talwar <gitanshutalwar@gmail.com>
2024-08-12 18:01:59 +0530

    test

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,26 @@
 import * as net from "net";
+enum Commands {
+  PING ="PING"
+
+}
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
+  connection.on("data", (data) => {
+    const command = data.toString().split(" ")[1].trim()
+    switch(command){
+      case Commands.PING:
+        connection.write("+PONG\r\n")
+        break;
+      default:
+        connection.write("-ERR unknown command '" + command + "'\r\n")
+    }
+  });
+
+
+  // connection.on("connectionAttempt")
 });
-//
 server.listen(6379, "127.0.0.1");
